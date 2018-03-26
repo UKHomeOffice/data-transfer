021137966e3c3f5f37c1fdb9169d862f80a479cf
james-rowley <jrowley79@gmail.com>
2018-03-26 11:04:05 +0100

    Updated v1.5.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 setup(
     name="data-transfer",
-    version="1.5.2",
+    version="1.5.3",
     author="Aker Systems",
     author_email="development@akersystems.com",
     classifiers=[
