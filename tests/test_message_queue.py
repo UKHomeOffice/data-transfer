5139e3637794ee9739c402affb4d1a81374410bf
James Lizamore <jwlizamore@gmail.com>
2018-08-24 17:13:41 +0100

    Added setup, event publishing test

--- tests/test_message_queue.py
+++ tests/test_message_queue.py
@@ -3,14 +3,29 @@ from datatransfer.storage import MessageQueue
 from unittest.mock import MagicMock
 
 class TestMessageQueue(unittest.TestCase):
+    def setup(self):
+        self.conf = {'username': 'user',
+                     'password': 'password',
+                     'host': 'rabbitmq',
+                     'port': '5672',
+                     'queue_name': 'a_test_queue'}
+        self.pika = MagicMock()
+        connection = MagicMock()
+        self.channel = MagicMock()
+        self.channel.basic_publish.return_value = True
+        connection.channel.return_value = self.channel
+        self.pika.BlockingConnection.return_value = connection
+        self.mq = MessageQueue(self.conf, pika=self.pika)
+
     def test_connection_establish(self):
-        conf = {'username': 'user',
-                'password': 'password',
-                'host': 'rabbitmq',
-                'port': '5672',
-                'queue_name': 'a_test_queue'}
-        mq = MessageQueue(conf)
+        self.setup()
+        mq = MessageQueue(self.conf)
         self.assertIsNotNone(mq.channel())
+
+    def test_event_publish(self):
+        self.setup()
+        self.assertTrue(self.mq.publish_event('an_event'))
+        self.channel.basic_publish.assert_called()
 
 if __name__ == "__main__":
     unittest.main()
